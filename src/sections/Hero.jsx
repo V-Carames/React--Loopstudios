aed3e3e08e72d80085a0dc42d486b27ca9a36aa8
VCarames <vcarames-dev@outlook.com>
2025-05-02 22:19:21 -0700

    Hero Section Styled and JSX Update

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,7 @@
 function Hero({ heading }) {
   return (
     <section className="hero-section">
-      <h1 className="hero-section__heading intro-heading">{heading}</h1>
+      <h1 className="hero-section__heading">{heading}</h1>
     </section>
   );
 }
