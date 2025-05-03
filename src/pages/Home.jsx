0a6ab2473069d367e0d35ab272b157bc16f2b906
VCarames <vcarames-dev@outlook.com>
2025-05-03 16:51:51 -0700

    "Creations" Section Created and Added To "Home" Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/header/Header";
 import About from "../sections/About";
+import Creations from "../sections/Creations";
 
 function Home() {
   return (
@@ -7,6 +8,7 @@ function Home() {
       <Header />
       <main>
         <About />
+        <Creations />
       </main>
     </>
   );
