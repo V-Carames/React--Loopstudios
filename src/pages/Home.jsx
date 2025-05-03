f3ad0197387de973e14f13b7008aa1b8ac6897cc
VCarames <vcarames-dev@outlook.com>
2025-05-03 16:08:12 -0700

    "About" Section Created and Added to "Home" Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import Header from "../components/header/Header";
+import About from "../sections/About";
 
 function Home() {
   return (
     <>
       <Header />
+      <main>
+        <About />
+      </main>
     </>
   );
 }
---
+++ src/sections/About.jsx
@@ -0,0 +1,27 @@
+import MobileImg from "/src/assets/images/mobile/image-interactive.jpg";
+import DesktopImg from "/src/assets/images/desktop/image-interactive.jpg";
+
+function About() {
+  return (
+    <section className="about-section">
+      <h2 className="visually-hidden">About Us</h2>
+      <picture className="about-section__picture">
+        <source media="(min-width: 62.5rem)" srcSet={DesktopImg} />
+        <img src={MobileImg} alt="" />
+      </picture>
+      <div className="about-section__content">
+        <h3 className="about-section__heading ">
+          The leader in interactive VR
+        </h3>
+        <p className="about-section__text ">
+          Founded in 2011, Loopstudios has been producing world-class virtual
+          reality projects for some of the best companies around the globe. Our
+          award-winning creations have transformed businesses through digital
+          experiences that bind to their brand.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
